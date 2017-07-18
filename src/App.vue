2144f26b74330c475f84acef4ce6be19b6457a06
mannysys <724475200@qq.com>
2017-07-18 22:02:19 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <!-- 路由渲染页面 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 
   export default {
     components: {
-      MHeader
+      MHeader,
+      Tab
     }
   }
 </script>
